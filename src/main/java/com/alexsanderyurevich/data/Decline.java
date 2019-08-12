53c62373557128b1a4490174db4d5735066349a7
Aleksandr_Starikov <AlexsandrYurevich@gmail.com>
2019-08-12 16:54:28 +0300

    --some changes

--- src/main/java/com/alexsanderyurevich/data/Decline.java
+++ src/main/java/com/alexsanderyurevich/data/Decline.java
@@ -8,11 +8,11 @@ import java.util.EnumMap;
 public final class Decline {
     private final EnumMap<Cases, String> declines = new EnumMap<>(Cases.class);
 
-    public void addDecline(@Nonnull Cases cases, @Nonnull String decline) {
+    void addDecline(@Nonnull Cases cases, @Nonnull String decline) {
         declines.put(cases,decline);
     }
 
-    public String getDecline(@Nonnull Cases cases) {
+    String getDecline(@Nonnull Cases cases) {
         return declines.get(cases);
     }
 
